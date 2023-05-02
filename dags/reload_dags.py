2389e1057305929393877fb55e8075bad365259a
me <21290320+my-oh-my@users.noreply.github.com>
2023-05-02 22:22:13 +0200

    Reload slash (#5)
    Co-authored-by: Rafał Krasowski <my-oh-my@users.noreply.github.com>

--- dags/reload_dags.py
+++ dags/reload_dags.py
@@ -9,7 +9,7 @@ dag = DAG('refresh-dags',
           default_args=get_default_args(),
           schedule_interval=None)
 
-command = f"cd {os.getenv('HOME')}airflow-processing && git fetch --all && git reset --hard origin/master"
+command = f"cd {os.getenv('HOME')}/airflow-processing && git fetch --all && git reset --hard origin/master"
 task = BashOperator(
     task_id='ea',
     bash_command=command,
