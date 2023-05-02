759f702df58f9f83e7a27d478deb0424f3837d3f
Rafał Krasowski <my-oh-my@users.noreply.github.com>
2023-05-02 22:02:15 +0200

    Introduce engulfing strategy

---
+++ dags/W20_consolidation_30.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+
+from processing import get_default_args
+
+with DAG('consolidation-W20-30',
+         default_args=get_default_args(),
+         schedule_interval='*/30 8-17 * * 1-5',
+         concurrency=1,
+         max_active_runs=1,
+         catchup=False) as dag:
+    args = f'{Variable.get("W20_CONSOLIDATION_30_ARGS")}'
+    command = f'cd /srv/git/expert-advisor/ea && python consolidation_runner.py ' + args
+    BashOperator(
+        task_id='ea',
+        bash_command=command,
+        dag=dag
+    )
---
+++ dags/W20_engulfing_60.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+
+from processing import get_default_args
+
+with DAG('engulfing-W20_60',
+          default_args=get_default_args(),
+          schedule_interval='0 8-18 * * 1-5',
+          concurrency=1,
+          max_active_runs=1,
+          catchup=False) as dag:
+    args = f'{Variable.get("W20_ENGULFING_60_ARGS")}'
+    command = f'cd /srv/git/expert-advisor/ea && python engulfing_runner.py ' + args
+    BashOperator(
+        task_id='ea',
+        bash_command=command,
+        dag=dag
+    )
